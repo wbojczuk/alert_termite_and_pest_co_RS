31d7c696fc8e89190dcad4d665baf637d04e9f76
William Bojczuk <williambojczuk@gmail.com>
2025-05-26 20:08:22 -0400

    update

--- src/app/(mainsite)/components/aboutpage/ServiceMap/ServiceMap.tsx
+++ src/app/(mainsite)/components/aboutpage/ServiceMap/ServiceMap.tsx
@@ -6,7 +6,9 @@ return (
  <div className={styles.serviceArea}>
     
     <div className={styles.mapWrapper}>
-    <iframe src="https://www.google.com/maps/d/embed?mid=1_q-CvgFqQXVcwPmDpmHYrwe4Om1KgJs&ehbc=2E312F" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <div className={styles.mapContainer}>
+          <iframe src="https://www.google.com/maps/d/embed?mid=1_q-CvgFqQXVcwPmDpmHYrwe4Om1KgJs&ehbc=2E312F" style={{"border":0}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
  </div>
 )};
